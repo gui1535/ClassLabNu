547a7a1ef5cc4b5e4c20566e5962e3ceac1f064f
gui1535 <guilherme.araujo1535@gmail.com>
2022-04-11 16:50:15 -0300

    Classes Usuario. Venda e Caixa

--- ClassLabNu/Usuario.cs
+++ ClassLabNu/Usuario.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Data;
+using System.Collections.Generic;
 
 namespace ClassLabNu
 {
@@ -13,9 +13,9 @@ namespace ClassLabNu
         public int Id { get; set; }
         public string Nome { get; }
         public string Email { get; set; }
-        public string Password { get;  }
+        public string Password { get; }
         public bool Ativo { get; set; }
-        public Nivel Nivel { get; }
+        public int Nivel { get; }
 
         // Metodos Construtores ------------------------------------------------------------------
 
@@ -24,7 +24,7 @@ namespace ClassLabNu
 
         }
 
-        public Usuario(string nome, string email, string password, Nivel nivel)
+        public Usuario(string nome, string email, string password, int nivel)
         {
             this.Nome = nome;
             this.Email = email;
@@ -33,7 +33,7 @@ namespace ClassLabNu
             this.Nivel = nivel;
         }
 
-        public Usuario(int id, string nome, string email, string password, bool ativo, Nivel nivel)
+        public Usuario(int id, string nome, string email, string password, bool ativo, int nivel)
         {
             this.Id = id;
             this.Nome = nome;
@@ -43,38 +43,93 @@ namespace ClassLabNu
             this.Nivel = nivel;
         }
 
+        public Usuario(int id, string nome, string email, string password, int nivel)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Password = password;
+            Nivel = nivel;
+        }
+
+
+
         // Metodos da Classe ---------------------------------------------------------------------
         public void Inserir()
         {
+            /*
             // Abre conexão com banco
             var cmd = Banco.Abrir();
-
+            // ---------------------------------------------------------------------//
             // Comandos SQL
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "insert Usuarios values(@id, @nome, @email, @senha)";
-
+            // ---------------------------------------------------------------------//
             // Parametros SQL
             cmd.Parameters.AddWithValue("@id", null);
             cmd.Parameters.AddWithValue("@nome", Nome);
             cmd.Parameters.AddWithValue("@email", Email);
-            cmd.Parameters.AddWithValue("@telefone", Password);
+            cmd.Parameters.AddWithValue("@senha", Password);
             cmd.Parameters.AddWithValue("@senha", Nivel);
             cmd.ExecuteNonQuery();
-
+            // ---------------------------------------------------------------------//
             // Pega o ultimo ID criado
             cmd.CommandText = "select @@identity";
-
+            // ---------------------------------------------------------------------//
             // Guarda o ultimo ID criado no Id
             Id = Convert.ToInt32(cmd.ExecuteScalar());
-
+            // ---------------------------------------------------------------------//
             // Limpar parametros
             cmd.Parameters.Clear();
+            */
+
+            // Abrindo conexão
+            var banco = Banco.Abrir();
+
+            // Inserindo dados
+            banco.CommandText = $"INSERT usuarios VALUES (null,'{Nome}', '{Email}', '{Password}', '{Nivel}');";
+            banco.ExecuteNonQuery();
+
+            // Pega o ultimo ID criado
+            banco.CommandText = @"select @@identity";
+
+            // Retorna o ID nas propridades
+            Id = Convert.ToInt32(banco.ExecuteScalar());
         }
 
         public static bool EfetuarLogin(string email, string senha)
         {
             // Realiza validação e devolve verdadeiro ou falso
             return false;
+        }
+
+        public static List<Usuario> ListarTodos()
+        {
+            // Listar usuario
+            List<Usuario> usuarios = new List<Usuario>();
+
+            // Abrindo conexão
+            var banco = Banco.Abrir();
+
+            // Comando SQL
+            banco.CommandText = "SELECT * FROM usuarios";
+
+            // var para consultar
+            var ConsultaDados = banco.ExecuteReader();
+
+            // Consulta
+            while (ConsultaDados.Read())
+            {
+                usuarios.Add(new Usuario(
+                        ConsultaDados.GetInt32("id"),
+                        ConsultaDados.GetString("nome"),
+                        ConsultaDados.GetString("email"),
+                        ConsultaDados.GetString("password"),
+                        ConsultaDados.GetBoolean("ativo"),
+                        ConsultaDados.GetInt32("nivel")
+                    ));
+            }
+            return usuarios;
         }
     }
 }
